20858a64a0cc1a1d8e70188855301427cf12ab45
Wama85 <wi_an9@hotmail.com>
2023-03-04 23:30:54 -0400

    nuevo

--- ahorcado.js
+++ ahorcado.js
@@ -1,59 +1,112 @@
-var palabras = ["javascript", "programacion", "computadora", "desarrollo", "tecnologia"];
-var palabra = palabras[Math.floor(Math.random() * palabras.length)];
-var letrasAdivinadas = [];
-var intentosRestantes = 6;
+// ### VARIABLES ###
 
-function mostrarPalabra() {
-    var palabraMostrada = "";
-    for (var i = 0; i < palabra.length; i++) {
-        if (letrasAdivinadas.indexOf(palabra[i]) !== -1) {
-            palabraMostrada += palabra[i] + " ";
-        } else {
-            palabraMostrada += "_ ";
-        }
-    }
-    document.getElementById("palabra").innerHTML = palabraMostrada;
+// Array de palabras
+var palabras = [["atlantico", "Un océano"], ["ordenador", "Una máquina"], ["laurel", "Un árbol"], ["plaza", "Espacio público"], ["rueda", "Gran invento"], ["cereza", "Una fruta"], ["petanca", "Un juego"], ["higuera", "Un árbol"], ["everest", "Un monte"], ["relampago", "Antecede al trueno"], ["jirafa", "Un animal"], ["luxemburgo", "Un país"], ["uruguay", "Un país"], ["ilustracion", "Representación gráfica"], ["excursion", "Actividad en la naturaleza"], ["empanadilla", "De la panadería"], ["pastel", "De la pastelería"], ["colegio", "Lugar para estudiar"], ["carrera", "Competición"], ["mermelada", "Confitura"]];
+// Palabra a averiguar
+var palabra = "";
+// Nº aleatorio
+var rand;
+// Palabra oculta
+var oculta = [];
+// Elemento html de la palabra
+var hueco = document.getElementById("palabra");
+// Contador de intentos
+var cont = 6;
+// Botones de letras
+var buttons = document.getElementsByClassName('letra');
+// Boton de reset
+var btnInicio = document.getElementById("reset");
+
+
+// ### FUNCIONES ###
+
+// Escoger palabra al azar
+function generaPalabra() {
+  rand = (Math.random() * 19).toFixed(0);
+  palabra = palabras[rand][0].toUpperCase();
+  console.log(palabra);
 }
 
-function mostrarLetras() {
-    document.getElementById("letras").innerHTML = letrasAdivinadas.join(", ");
+// Funcion para pintar los guiones de la palabra
+function pintarGuiones(num) {
+  for (var i = 0; i < num; i++) {
+    oculta[i] = "_";
+  }
+  hueco.innerHTML = oculta.join("");
 }
 
-function mostrarIntentos() {
-    document.getElementById("intentos").innerHTML = intentosRestantes;
+//Generar abecedario
+function generaABC (a,z) {
+  document.getElementById("abcdario").innerHTML = "";
+  var i = a.charCodeAt(0), j = z.charCodeAt(0);
+  var letra = "";
+  for( ; i<=j; i++) {
+    letra = String.fromCharCode(i).toUpperCase();
+    document.getElementById("abcdario").innerHTML += "<button value='" + letra + "' onclick='intento(\"" + letra + "\")' class='letra' id='"+letra+"'>" + letra + "</button>";
+    if(i==110) {
+      document.getElementById("abcdario").innerHTML += "<button value='Ñ' onclick='intento(\"Ñ\")' class='letra' id='"+letra+"'>Ñ</button>";
+    }
+  }
 }
 
-function adivinar() {
-    var letra = document.getElementById("letra").value.toLowerCase();
-    if (letra.length !== 1 || letrasAdivinadas.indexOf(letra) !== -1) {
-        return;
+// Chequear intento
+function intento(letra) {
+  document.getElementById(letra).disabled = true;
+  if(palabra.indexOf(letra) != -1) {
+    for(var i=0; i<palabra.length; i++) {
+      if(palabra[i]==letra) oculta[i] = letra;
     }
-    letrasAdivinadas.push(letra);
-    if (palabra.indexOf(letra) === -1) {
-        intentosRestantes--;
-    }
-    mostrarPalabra();
-    mostrarLetras();
-    mostrarIntentos();
-    if (intentosRestantes === 0) {
-        alert("Perdiste. La palabra era " + palabra + ".");
-        location.reload();
-    }
-    if (palabraAdivinada()) {
-        alert("Ganaste!");
-        location.reload();
-    }
+    hueco.innerHTML = oculta.join("");
+    document.getElementById("acierto").innerHTML = "Bien!";
+    document.getElementById("acierto").className += "acierto verde";
+  }else{
+    cont--;
+    document.getElementById("intentos").innerHTML = cont;
+    document.getElementById("acierto").innerHTML = "Fallo!";
+    document.getElementById("acierto").className += "acierto rojo";
+    document.getElementById("image"+cont).className += "fade-in";
+  }
+  compruebaFin();
+  setTimeout(function () { 
+    document.getElementById("acierto").className = ""; 
+  }, 800);
 }
 
-function palabraAdivinada() {
-    for (var i = 0; i < palabra.length; i++) {
-        if (letrasAdivinadas.indexOf(palabra[i]) === -1) {
-            return false;
-        }
-    }
-    return true;
+// Obtener pista
+function pista() {
+  document.getElementById("hueco-pista").innerHTML = palabras[rand][1];
 }
 
-mostrarPalabra();
-mostrarLetras();
-mostrarIntentos();
+// Compruba si ha finalizado
+function compruebaFin() {
+  if( oculta.indexOf("_") == -1 ) {
+    document.getElementById("msg-final").innerHTML = "Felicidades !!";
+    document.getElementById("msg-final").className += "zoom-in";
+    document.getElementById("palabra").className += " encuadre";
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+    document.getElementById("reset").innerHTML = "Empezar";
+    btnInicio.onclick = function() { location.reload() };
+  }else if( cont == 0 ) {
+    document.getElementById("msg-final").innerHTML = "Fin del Juego";
+    document.getElementById("msg-final").className += "zoom-in";
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+    document.getElementById("reset").innerHTML = "Empezar";
+    btnInicio.onclick = function () { location.reload() };
+  }
+}
+
+// Restablecer juego
+function inicio() {
+  generaPalabra();
+  pintarGuiones(palabra.length);
+  generaABC("a","z");
+  cont = 6;
+  document.getElementById("intentos").innerHTML=cont;
+}
+
+// Iniciar
+window.onload = inicio();
